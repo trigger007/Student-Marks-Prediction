db7830cb2cf3b27b0c764f79fbafdbd476dd8376
trigger007 <aditya1999nov@gmail.com>
2020-05-30 19:23:04 +0530

    math complete

--- student_math_reg.py
+++ student_math_reg.py
@@ -25,9 +25,8 @@ data=pd.read_excel("stud-mat.xlsx")
 print(data)
 X= data.iloc[:, :-3].values
 print(X)
-y1=data.iloc[:,30:31].values
-y3=data.iloc[:,31:32].values
-y2=data.iloc[:,32].values
+y=data.iloc[:, -3:].values
+
 
 le=LabelEncoder()   
 X[:,0]=le.fit_transform(X[:, 0])
@@ -48,7 +47,7 @@ X[:,22]=le.fit_transform(X[:, 22])
 for i in range(28,36):
     X[:,i]=le.fit_transform(X[:, i])
     
-X_train, X_test, y_train, y_test = train_test_split(X, y1, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
@@ -65,7 +64,7 @@ ann.add(Dense(units = 1))
 
 ann.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['accuracy'])
 
-ann.fit(X_train, y_train, batch_size = 32, epochs = 100)
+ann.fit(X_train, y_train[:,0], batch_size = 32, epochs = 100)
 
 y_pred = ann.predict(X_test)
 
@@ -79,24 +78,31 @@ for i in range(len(y_pred_flat)):
 
 y1_pred=np.array(y1_pred) 
 
-rmse = math.sqrt(mean_squared_error(y_test, y1_pred))
+rmse1 = math.sqrt(mean_squared_error(y_test[:,0], y1_pred))
 print(rmse)
 
 np.set_printoptions(precision=2)
-k=np.concatenate((y1_pred.reshape(len(y1_pred),1), y_test.reshape(len(y_test),1)),1)
+k1=np.concatenate((y1_pred.reshape(len(y1_pred),1), y_test[:,0].reshape(len(y_test[:,0]),1)),1)
+
+X_test=sc.inverse_transform(X_test)
+X_test[X_test<0] = 0
+
+y1_pred=y1_pred.reshape(len(y1_pred),1)                 
+
+X_test=np.concatenate((X_test,y1_pred),axis=1)
+y1=np.array(y_train[:,0])
+y1=y1.reshape(len(y1),1)
+X_train=sc.inverse_transform(X_train)
+X_train=np.concatenate((X_train,y1),axis=1)
+X_train[X_train<0] = 0
 
 #NEURAL NETWORK PART 2
 
-from sklearn.model_selection import train_test_split
-X_train1, X_test1, y_train1, y_test1 = train_test_split(X, y3, test_size = 0.2, random_state = 0)
-
-X_testf=X_test1
-X_trainf=X_train1
 
 
 sc1 = StandardScaler()
-X_train1 = sc1.fit_transform(X_train)
-X_test1 = sc1.transform(X_test)
+X_train[:,:-1] = sc1.fit_transform(X_train[:,:-1])
+X_test[:,:-1] = sc1.transform(X_test[:,:-1])
 
 ann1 = Sequential()
 
@@ -108,36 +114,82 @@ ann1.add(Dense(units = 1))#softmax when more than 2 classification
 
 ann1.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['accuracy'])
 
-ann1.fit(X_train1, y_train1, batch_size = 32, epochs = 100)
+ann1.fit(X_train[:,:-1], y_train[:,1], batch_size = 32, epochs = 100)
 
-y_pred1 = ann1.predict(X_test1)
+y_pred = ann1.predict(X_test[:,:-1])
 
-y_pred1_flat=y_pred.flatten()
+y_pred_flat=y_pred.flatten()
 
-#ARRANGING THE ARRAYS 2
+#ARRANGING THE ARRAYS
 
-y1_pred1=[]
-for i in range(len(y_pred1_flat)):
-    y1_pred1.append(math.ceil(y_pred1_flat[i]))
-    
-y1_pred1=np.array(y1_pred1)    
-    
-from sklearn.metrics import mean_squared_error
-rmse1 = math.sqrt(mean_squared_error(y_test1, y1_pred1))
-print(rmse1)
+y1_pred=[]
+for i in range(len(y_pred_flat)):
+    y1_pred.append(math.floor(y_pred_flat[i]))
+
+y1_pred=np.array(y1_pred) 
+
+rmse2 = math.sqrt(mean_squared_error(y_test[:,1], y1_pred))
+print(rmse)
 
 np.set_printoptions(precision=2)
-k1=np.concatenate((y1_pred1.reshape(len(y1_pred1),1), y_test1.reshape(len(y_test1),1)),1)
+k2=np.concatenate((y1_pred.reshape(len(y1_pred),1), y_test[:,1].reshape(len(y_test[:,1]),1)),1)
 
-#PREPROCESS 3
+X_test[:,:-1]=sc.inverse_transform(X_test[:,:-1])
+X_test[:,:-1][X_test[:,:-1]<0.2] = 0
 
-Xf=np.concatenate((X_trainf,y_train),axis=1)
-Xf1=np.concatenate((Xf,y_train1),axis=1)
+y1_pred=y1_pred.reshape(len(y1_pred),1)                 
 
-X= data.iloc[:, :-1].values
-Y= data.iloc[:, -1].values
+X_test=np.concatenate((X_test,y1_pred),axis=1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y1, test_size = 0.2, random_state = 0)
+y1=np.array(y_train[:,1])
+y1=y1.reshape(len(y1),1)
+X_train[:,:-1]=sc.inverse_transform(X_train[:,:-1])
+X_train=np.concatenate((X_train,y1),axis=1)
+X_train[X_train<0] = 0
+
+
+#NEURAL NETWORK PART 3
+
+sc1 = StandardScaler()
+X_train = sc1.fit_transform(X_train)
+X_test = sc1.transform(X_test)
+
+ann2 = Sequential()
+
+ann2.add(Dense(units = 20, activation = 'relu'))
+
+ann2.add(Dense(units = 20, activation = 'relu'))
+
+ann2.add(Dense(units = 1))#softmax when more than 2 classification
+
+ann2.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = ['accuracy'])
+
+ann2.fit(X_train, y_train[:,2], batch_size = 32, epochs = 100)
+
+y_pred = ann2.predict(X_test)
+
+y_pred_flat=y_pred.flatten()
+
+#ARRANGING THE ARRAYS
+
+y1_pred=[]
+for i in range(len(y_pred_flat)):
+    y1_pred.append(math.floor(y_pred_flat[i]))
+
+y1_pred=np.array(y1_pred) 
+
+rmse3 = math.sqrt(mean_squared_error(y_test[:,2], y1_pred))
+print(rmse)
+
+np.set_printoptions(precision=2)
+k3=np.concatenate((y1_pred.reshape(len(y1_pred),1), y_test[:,2].reshape(len(y_test[:,2]),1)),1)
+
+
+
+
+
+
+
 
 
 
